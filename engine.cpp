39fc8cc97cce8d2363163e47378ddbf7b474552d
sysop1021 <mattwbrown@gmail.com>
2018-10-18 15:03:33 -0500

    changed speed - no real changes
    Getting strange flickering of sprite and bg... unsure what's going on

--- engine.cpp
+++ engine.cpp
@@ -32,24 +32,24 @@ void Engine::HandleInput(sf::Transformable& transformable)
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) ||sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
         // move sprite up
-        transformable.move(0.0, -4.0);
+        transformable.move(0.0f, -8.0f);
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) ||sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
     {
         // down
-        transformable.move(0.0, 4.0);
+        transformable.move(0.0f, 8.0f);
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ||sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
         // left
-        transformable.move(-4.0, 0.0);
+        transformable.move(-8.0f, 0.0f);
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ||sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
         // right
-        transformable.move(4.0, 0.0);
+        transformable.move(8.0f, 0.0f);
     }
 }
